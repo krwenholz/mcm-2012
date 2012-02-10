41b375c7617e316e8b910b81474fbbf2edba9e84
Kyle Wenholz <kylewenholz@gmail.com>
2012-02-10 14:59:49 -0800

    Started making the directory tree and adding in a slew of some interfaces and such for the coding.

--- Code/MCM-2012/src/iAttribute.java
+++ Code/MCM-2012/src/iAttribute.java
@@ -1,4 +1,33 @@
 
-public interface iAttribute {
+public interface iAttribute<E> {
+
+    /**
+     * Get a string describing the attribute domain (basically type).
+     **/
+    String getDomainDescription();
+
+    /**
+     * Get a list of valid domain values.
+     **/
+    <E>[] getDomain();
+
+    /**
+     * Returns the current attribute value.
+     **/
+    <E> getValue();
+
+    /**
+     * Returns attributes this one depends on.
+     **/
+    iAttribute[] getDependencies();
+
+    /**
+     * Add a dependency.
+     **/
+    void addDependency(iAttribute<E> newAttr);
+
+    /**
+     * Alter domain
+     **/
 
 }
