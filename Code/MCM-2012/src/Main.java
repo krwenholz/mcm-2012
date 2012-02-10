be766bc2093045aeb51458207d64594c64b1fd3f
Kyle Wenholz <kylewenholz@gmail.com>
2012-02-10 14:36:55 -0800

    Started making the directory tree and adding in a slew of some interfaces and such for the coding.

---
+++ Code/MCM-2012/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	/**
+	 * The main method should initialize variables we want to set for
+	 * absolutely ANYTHING.  This way we can avoid screwing around with
+	 * multiple files.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	    System.out.println("Hello World.");
+	}
+
+}
---
+++ Code/MCM-2012/src/iVariable.java
@@ -0,0 +1,39 @@
+/**
+ * Defines the methods a variable (something to be constrained) must have,
+ * along with the general behavior descriptions for these methods.
+ **/
+public interface iVariable {
+
+    /**
+     * Returns a string representation of this variable's names and various
+     * states.
+     **/
+    String toString();
+
+    /**
+     * Returns the domain values for the requested variable attribute.
+     **/
+    iAttribute getDomainValue(String variableAttribute);
+
+    /**
+     * Returns the current value of the variable attribute specified.
+     **/
+    iAttribute getValue(String variableAtrribute);
+
+    /**
+     * Returns a list, String representation, of the attributes in this
+     * variable.
+     **/
+    String[] getValues();
+
+    /**
+     * Returns a list of other variables this one is dependent on.
+     **/
+    iVariable[] getDependencies();
+
+    /**
+     * Adds a variable to the dependency list.
+     **/
+    void addDependency(iVariable newDep);
+
+}
