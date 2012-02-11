809e81e3ff2c9b7f3bcfe70046a26eb10b029b67
Kyle Wenholz <kylewenholz@gmail.com>
2012-02-11 01:11:54 -0800

    something

--- Code/MCM-2012/src/ConstraintSatisfaction.java
+++ Code/MCM-2012/src/ConstraintSatisfaction.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * The heavy lifting class.  Runs the primary search algorithm, heuristic
  * assesments, and various other high level solution processes.
@@ -35,7 +37,8 @@ public class ConstraintSatisfaction {
 
     //THIS GUY WILL BE DONE EACH DAY, THE CALLER SHOULD CONSTRUCT THE ASSIGNMENT
     // AND CSP SUCH THAT IT IS LIMITED TO WHAT IT NEEDS TO DO THAT DAY
-    public ArrayList<TravellingAssignment> backtrackingRecurse(TravellingAssignment ass, CSP_Scheduling csp){
+    public ArrayList<TravellingAssignment> backtrackingRecurse(
+    		TravellingAssignment ass, CSP_Scheduling csp){
 	//check if everything is assigned (we have already been checking for correctness)
 	if(csp.isComplete()){
 	    //FORMULATE A NEW CSP FOR DAY me++
@@ -43,38 +46,21 @@ public class ConstraintSatisfaction {
 	    //RETURN IT OR NOT
 	}
 	//select the next unassigned variable
-	String var = selectUnassignedVar(csp.getVariables(), ass, csp);
-	for(String val: orderDomainValues(var, ass, csp)){
+	TravelGroup g = csp.getNextGroup();
+	for(int val: csp.getCampsites(g, ass)){
 	    //attempt to assign domain values to this variable
-	    if(isConsistent(val, ass, csp)){
+	    if(ass.isConsistent(val)){
 		//if the assignment was valid so far then here we go
-		ass.add(var,val);
+		ass.add(val, g);
 		//HERE WE SHOULD CHECK OUR ASSIGNMENT WITH FILTERING OR SOMETHING
 		ArrayList<TravellingAssignment> isSuccess = backtrackingRecurse(ass, csp);
 		if(isSuccess){
 		    return ass;
-		}ass.remove(var,val);
+		}ass.remove(val);
 	    }
 	}
 	//we failed to create a valid assignment
+	csp.addBack(g);
 	return null;
     }
-
-
-    /**
-     * Returns an unassigned variable that should get an assignment next.
-     **/
-    public Variable selectUnassignedVar(Variables[] vars, TravellingAssignment ass, CSP_Scheduling csp){
-
-    }
-
-    /**
-     * Orders the possible values for a given variable and returns an array
-     * of these.
-     **/
-    public Value orderDomainValues(Variable var, TravellingAssignment ass, CSP_Scheduling csp){
-
-    }
-
-
 }
